60ebe5781236ec17f4199399e362d9b0ee3a212f
Ethan Estrada <ethan.estrada@gmail.com>
2012-09-22 19:54:29 -0600

    Modified main.cpp to actually test for something

--- lab4/Student_Code/main.cpp
+++ lab4/Student_Code/main.cpp
@@ -8,10 +8,9 @@ using namespace std;
 int main()
 {
     ExpressionManager emtest;
-    std::string teststr = "Let's check if this works.";
+    string teststr = "Let's check if this works.";
 
-    emtest.isBalanced(teststr);
-    std::cout<<"This program has run.\n";
-    std::cout<<"Soon it will actually do something useful."<<std::endl;
+    bool test = emtest.isBalanced(teststr);
+    cout << "Returned value: " << boolalpha << test << endl;
     return 0;
 }
