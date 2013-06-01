5c2bb45cbe1ce721ffa247ea5e7125fa8f204265
Ethan Estrada <ethan.estrada@gmail.com>
2013-06-01 01:11:51 -0600

    Added base files for midterm
    Started on deque class since it should be possible to use to implement
    pretty much every other class that is require for the midterm.

---
+++ cs235/midterm/deque.cpp
@@ -0,0 +1,115 @@
+#include "deque.h"
+
+
+template < typename T >
+deque<T>::deque() : capacity(DEFAULT_CAPACITY), num_items(0), front_index(0),
+    rear_index(0), the_data(new T[DEFAULT_CAPACITY]) {}
+
+template < typename T >
+void deque<T>::reallocate()
+{
+    size_t new_capacity = 2 * capacity;
+    T *new_data = new T[new_capacity];
+    size_t j = front_index;
+    for(size_t i = 0; i < num_items; ++i)
+    {
+        new_data[i] = the_data[j];
+        j = (j + 1) % capacity;
+    }
+
+    front_index = 0;
+    rear_index = num_items - 1;
+    capacity = new_capacity;
+
+    T *tmp_ptr = the_data; the_data = new_data;
+
+    delete[] tmp_ptr;
+}
+
+template < typename T >
+void deque<T>::push_back(const T& data)
+{
+    if(num_items == capacity) this->reallocate();
+
+    ++num_items;
+    rear_index = (rear_index + 1) % capacity;
+    the_data[rear_index] = item;
+}
+
+template < typename T >
+void deque<T>::pop_front()
+{
+    front_index = (front_index + 1) % capacity;
+    --num_items;
+}
+
+template < typename T >
+void deque<T>::pop_back();
+
+//template < typename T >
+//T deque<T>::pop_front(int);
+
+//template < typename T >
+//T deque<T>::pop_back(int);
+
+template < typename T >
+const T & deque<T>::operator[](size_t index) const;
+
+template < typename T >
+T & deque<T>::operator[](size_t index);
+
+template < typename T >
+const T & deque<T>::at(size_t index) const;
+
+template < typename T >
+T & deque<T>::at(size_t index);
+
+template < typename T >
+const T & deque<T>::front() const
+{
+    return the_data[front_index];
+}
+
+template < typename T >
+T & deque<T>::front()
+{
+    return the_data[front_index];
+}
+
+template < typename T >
+const T & deque<T>::back() const
+{
+    return the_data[rear_index];
+}
+
+template < typename T >
+T & deque<T>::back()
+{
+    return the_data[rear_index];
+}
+
+template < typename T >
+void deque<T>::swap(Deque<T> &other)
+{
+    throw false;
+}
+
+template < typename T >
+bool deque<T>::empty() const
+{
+    return this->num_items == 0;
+}
+
+
+template < typename T >
+size_t deque<T>::size() const
+{
+    return num_items;
+}
+
+template < typename T >
+void deque<T>::resize(size_t size)
+{
+    throw false;
+}
+
---
+++ cs235/midterm/deque.h
@@ -0,0 +1,46 @@
+#ifndef _DEQUE_H_
+#define _DEQUE_H_
+
+#include <stdexcept>
+
+template < typename T >
+class deque
+{
+public:
+    deque();
+    ~deque();
+
+    const T & operator[](size_t index) const;
+    T & operator[](size_t index);
+
+    const T & at(size_t index) const;
+    T & at(size_t index);
+
+    void push_front(const T& data);
+    void push_back(const T& data);
+    void pop_front();
+    void pop_back();
+    T pop_front(int);
+    T pop_back(int);
+
+    const T & front() const;
+    T & front();
+
+    const T & back() const;
+    T & back();
+
+    void swap(Deque<T> &other);
+    bool empty() const;
+    size_t size() const;
+    void resize(size_t size);
+private:
+    void reallocate();
+
+private:
+    static const size_t DEFAULT_CAPACITY = 8;
+
+    size_t capacity, num_items, front_index, rear_index;
+    T *the_data;
+};
+
+#endif // Defined _DEQUE_H_
