bdaa9ac1a50d35bcee89669efab7279a1a44e10b
Ethan Estrada <ethan.estrada@gmail.com>
2013-06-03 02:13:57 -0600

    cs235/midterm. More work on test driver

--- cs235/midterm/src/main.cpp
+++ cs235/midterm/src/main.cpp
@@ -48,7 +48,7 @@ int main(int argc, char **argv)
 
     }
 
-    std::cerr << "End of program." << std::endl;
+    //std::cerr << "End of program." << std::endl;
 
     return retval;
 }
--- cs235/midterm/src/main.h
+++ cs235/midterm/src/main.h
@@ -5,17 +5,16 @@
 #include <exception>
 #include <stdexcept>
 #include <iostream>
-#include <vector>
 #include <string>
 #include <cstdlib>
 
-class not_implemented : public std::exception
+class not_implemented : public std::runtime_error
 {
 public:
     not_implemented(const std::string &s = "not implemented") :
-        val(s) {}
+        std::runtime_error(s){}//val(s) {}
     virtual ~not_implemented() throw() {}
-    virtual const char* what() const throw() { return val.c_str(); }
+    //virtual const char* what() const throw() { return val.c_str(); }
 private:
     std::string val;
 };
@@ -35,15 +34,32 @@ inline void test_deque()
     deque<int> d;
     std::cerr << "Appending 0 to 500 by 5s to the back end.\n";
 
-    for(size_t i = 0; i < 500; i+=5)
+    for(size_t i = 0; i < 100; i+=5)
     {
-        d.push_back(i);
+        if (i % 2 == 0)
+            d.push_back(i);
+        else
+            d.push_front(i);
+            
     }
 
-    for(size_t i = 0; i < d.size(); ++i)
+    for(size_t i = 0; i < 20; ++i)
     {
-        std::cerr << "Index "<< i << ": " << d[i] << "\n";
+        //if (d.at(i) != i * 5)
+        if (false)
+        {
+            std::clog << "Index "<< i << " does not match expected result.";
+            std::clog << " Returned: " << d[i] << ". Should have returned: ";
+            std::clog << (i * 5) << ".\n";
+            std::clog << "Deque test failed!\n\n";
+            std::clog << std::flush;
+            return;
+        }
+        else
+            std::clog << "Index "<< i << ": " << d[i] << "\n";
     }
+
+    std::clog << "Deque test passed!\n\n";
     std::clog << std::flush;
 }
 
@@ -59,15 +75,23 @@ inline void test_or_deque()
 
 inline bool test_all()
 {
+    bool passed = true;
     try
     {
+        test_stack();
+        test_queue();
         test_deque();
+        test_ir_deque();
+        test_or_deque();
     }
-    catch(std::exception &e)
+    catch(not_implemented &e)
     {
+        std::clog << "Exception encountered: " << e.what() << std::endl;
+        passed = false;
     }
 
-    return bool();
+    return passed;
 }
 
 #endif // Defined _MAIN_H_
+
