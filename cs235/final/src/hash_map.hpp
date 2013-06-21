536df9515d24cc205aaa1be6ffa2dd6b732b03b9
Ethan Estrada <ethan.estrada@gmail.com>
2013-06-20 23:31:30 -0600

    cs235/final. working on custom hash_table
    Even though the exam is done, this is for my own learning and
    benefit. I really want to understand hash tables.

--- cs235/final/src/hash_map.hpp
+++ cs235/final/src/hash_map.hpp
@@ -1,6 +1,10 @@
 #if !defined(_HASH_MAP_HPP_)
 #define _HASH_MAP_HPP_
 
+#include "deque.hpp"
+//#include "list.hpp"
+#include <vector>
+#include <list>
 #include "utility.hpp"
 
 namespace ede
@@ -8,18 +12,36 @@ namespace ede
 
 template <  typename Key,
             typename Val,
-            typename Compare = less<Key> >
+            typename Hash = ::ede::hash<Key> >
 class hash_map
 {
-public:
-    typedef pair< Key, Val > Entry_type;
+public: // Member typedefs
     typedef Key key_type;
     typedef Val value_type;
-    typedef Compare key_compare;
+    typedef pair< key_type, value_type > entry_type;
+    typedef Hash hash_function_type;
 
-public:
+public: // Public member types
+    class iterator;
+    class const_iterator;
 
-protected:
+public: //Public member functions
+    hash_map();
+    ~hash_map();
+
+    bool erase(const key_type &k);
+
+    pair<iterator, bool> insert(const entry_type &entry);
+    pair<iterator, bool> insert(const key_type &k, const value_type &v);
+protected: // Protected member functions
+protected: // Protected member variables
+    /* The hash function object. */
+    hash_function_type hash_fcn;
+    size_t num_keys;
+    ede::deque< std::list<entry_type> > the_buckets;
+    static size_t INITIAL_CAPACITY;
+    static const double LOAD_THRESHOLD;
+    
 };
 
 }
--- cs235/final/src/utility.hpp
+++ cs235/final/src/utility.hpp
@@ -1,6 +1,8 @@
 #if !defined(_UTILITY_HPP_)
 #define _UTILITY_HPP_
 
+#include <string>
+
 namespace ede
 {
 
@@ -36,6 +38,35 @@ struct greater
     bool operator()(const T &a, const T &b) const
     {
         return (a > b);
+    }
+};
+
+
+/*
+ * Generic hash for numeric types. Equivalent numeric types should evaluate equal.
+ * (e.g. 0.0 = 0 = 0L = 0.0f)
+ */
+template < typename T >
+struct hash
+{
+    size_t operator()(const T &var) const
+    {
+        return static_cast<size_t>(4262999287U * var);
+    }
+};
+
+/*
+ * Hash specialization for strings.
+ */
+template <>
+struct hash<std::string>
+{
+    size_t operator()(const std::string &s) const
+    {
+        size_t result = 0;
+        for(size_t i = 0; i < s.length(); ++i)
+            result = result * 31 + s[i];
+        return result;
     }
 };
 
