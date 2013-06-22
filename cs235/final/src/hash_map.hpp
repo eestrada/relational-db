7d00267a9412e3c7d329ba42e63a84a6521376ab
Ethan Estrada <ethan.estrada@gmail.com>
2013-06-21 20:13:49 -0600

    cs235/final. More work on hash_map interface

--- cs235/final/src/hash_map.hpp
+++ cs235/final/src/hash_map.hpp
@@ -1,11 +1,12 @@
 #if !defined(_HASH_MAP_HPP_)
 #define _HASH_MAP_HPP_
 
-#include "deque.hpp"
+//#include "deque.hpp"
 //#include "list.hpp"
 #include <vector>
 #include <list>
 #include "utility.hpp"
+#include <utility>
 
 namespace ede
 {
@@ -18,7 +19,7 @@ class hash_map
 public: // Member typedefs
     typedef Key key_type;
     typedef Val value_type;
-    typedef pair< key_type, value_type > entry_type;
+    typedef std::pair< key_type, value_type > entry_type;
     typedef Hash hash_function_type;
 
 public: // Public member types
@@ -26,22 +27,32 @@ public: // Public member types
     class const_iterator;
 
 public: //Public member functions
-    hash_map();
-    ~hash_map();
+    hash_map() : hash_fcn(), num_keys(0), the_buckets(INITIAL_CAPACITY), LOAD_THRESHOLD(3.0) {}
+    void clear();
+
+    std::pair<iterator, bool> insert(const entry_type &entry);
+    std::pair<iterator, bool> insert(const key_type &k, const value_type &v);
 
     bool erase(const key_type &k);
 
-    pair<iterator, bool> insert(const entry_type &entry);
-    pair<iterator, bool> insert(const key_type &k, const value_type &v);
+    value_type & operator[](const key_type &k);
+    const value_type & operator[](const key_type &k) const;
+
+    value_type & at(const key_type &k);
+    const value_type & at(const key_type &k) const;
+
 protected: // Protected member functions
 protected: // Protected member variables
     /* The hash function object. */
     hash_function_type hash_fcn;
+    /* The number of keys (not hashes) in the map. */
     size_t num_keys;
-    ede::deque< std::list<entry_type> > the_buckets;
-    static size_t INITIAL_CAPACITY;
-    static const double LOAD_THRESHOLD;
-    
+    /* Buckets to hold linked lists with values. */
+    std::vector< std::list<entry_type> > the_buckets;
+
+    /* Useful constants. */
+    const double LOAD_THRESHOLD;
+    static const size_t INITIAL_CAPACITY = 100;
 };
 
 }
