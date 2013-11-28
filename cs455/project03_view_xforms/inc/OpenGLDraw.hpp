f399e2a586aabe7f2d7a970e37374f158088ddd0
Ethan Estrada <ethan.estrada@gmail.com>
2013-11-27 23:00:58 -0700

    cs455/project03. Start making project more OOP

---
+++ cs455/project03_view_xforms/inc/OpenGLDraw.hpp
@@ -0,0 +1,29 @@
+#if !defined(OPENGLDRAW_HPP)
+#define OPENGLDRAW_HPP
+
+/* Include OpenGL headers */
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glext.h>
+
+#include "utils/exceptions.hpp"
+#include "cg/objparser.hpp"
+#include "cg/image.hpp"
+#include "utils/system.hpp"
+
+/* Classes */
+
+class OpenGLDraw
+{
+};
+
+class GLUTDraw
+{
+    GLUTDraw(){}
+
+    void init(int width, int height)
+};
+
+#endif // end include guard
+
