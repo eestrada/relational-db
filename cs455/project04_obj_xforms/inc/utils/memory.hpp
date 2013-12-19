9fd45552b13369c3386125cbd73aeb28fc991aac
Ethan Estrada <ethan.estrada@gmail.com>
2013-12-19 11:15:13 -0700

    cs455/project4. Start my own shared pointer class
    This is to replace the C++11 version of shared pointer, which I use
    pretty extensively in my code.

---
+++ cs455/project04_obj_xforms/inc/utils/memory.hpp
@@ -0,0 +1,20 @@
+#if !defined(MEMORY_HPP)
+#define MEMORY_HPP
+
+namespace ptr
+{
+
+template <typename P>
+class shared_ptr
+{
+public: //functions
+shared_ptr(){}
+~shared_ptr(){}
+public: //variables
+protected: //functions
+protected: //variables
+};
+
+} // End namespace ptr
+
+#endif // end include guard
