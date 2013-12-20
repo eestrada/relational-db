d80c231d4dc5288ad52e564c0d9e74fe6dbc70ae
Ethan Estrada <ethan.estrada@gmail.com>
2013-12-19 21:43:58 -0700

    cs455/project4. More shared pointer stuff

--- cs455/project04_obj_xforms/inc/utils/memory.hpp
+++ cs455/project04_obj_xforms/inc/utils/memory.hpp
@@ -2,17 +2,23 @@
 #define MEMORY_HPP
 
 namespace ptr
+//namespace std
 {
 
-template <typename P>
+template <typename T>
 class shared_ptr
 {
+public: //typedefs
 public: //functions
-shared_ptr(){}
-~shared_ptr(){}
+    shared_ptr(){}
+    template <typename U>
+    explicit shared_ptr(U* p) : ptr(p), counter(new size_t()){}
+    ~shared_ptr(){}
 public: //variables
 protected: //functions
 protected: //variables
+    T* ptr;
+    size_t* counter;
 };
 
 } // End namespace ptr
