b9acbe96e3ea330f541b3c4920545238b4305c16
Ethan Estrada <ethan.estrada@gmail.com>
2014-06-29 00:41:59 -0600

    Add more test cases
    All tests pass with the code in its current state.
    
    TODO: Add even more test cases

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ mkdir -p ./output
 mkdir -p ./diffs
 
 #for i in 1 2 3 4 5 6 7 8 9 A B C D E F
-for i in 0 1 2 3 4 5
+for i in 0 1 2 3 4 5 6 7 8 9
 do
     ./test.bin ./tests/in1${i}.txt ./output/actual1${i}.txt
     diff -su ./tests/out1${i}.txt ./output/actual1${i}.txt >> ./diffs/diff1.diff
