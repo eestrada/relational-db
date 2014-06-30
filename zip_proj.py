04fffa712e878277ff104b1d92d1d446983b5e1a
Ethan Estrada <ethan.estrada@gmail.com>
2014-06-30 16:06:18 -0600

    Add simple python script to help with zipping up files

---
+++ zip_proj.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+import os, sys, zipfile, glob
+
+def main(args):
+    paths = glob.glob('./src/*.cpp')
+    paths += glob.glob('./inc/*.h')
+    with zipfile.ZipFile(args[1], mode='w') as zf:
+        for p in paths:
+            name = p.split('/')[-1]
+            zf.write(p, name)
+
+if __name__ == '__main__':
+    ret = main(sys.argv)
+    sys.exit(ret)
