69dde19e9feb7333e94398a487aea8a687484082
Ethan Estrada <ethan.estrada@gmail.com>
2014-11-20 21:22:34 -0700

    Remove timing code from main
    If it is needed again, "git revert" can be used to add the code back
    in.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
 #include "Driver.h"
-#include <ctime>
-#include <iostream>
 
 int main(int argc, char **argv)
 {
-	std::clock_t t;
-	t = std::clock();
-    auto retval = Driver::interpret(argc, argv);
-    t = std::clock() - t;
-    std::cout << "Took me " << t << " clicks (around " << float(t)/CLOCKS_PER_SEC << " seconds).\n";
-    return retval;
+    return Driver::interpret(argc, argv);
 }
 
